926b72caf36c0b50b8b69ef87a186ef8ad0bf319
Alexandra Pestruyeva <apestruy@gmail.com>
2019-11-29 05:27:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -14,21 +14,21 @@
 #   }
 # }
 # 
- PORTION_2 = {
-   label: "Order"
- }
+# PORTION_2 = {
+#   label: "Order"
+# }
  
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
 # 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
   hash = PORTION_1, PORTION_2, PORTION_3, PORTION_4 
